f5983ac99a398960438be8f971148ba22c46ffb2
Ketosvaldo <ketchulo.conk2013@gmail.com>
2022-04-04 09:08:12 -0700

    Subir examen

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
-import logo from '../logo.svg';
 import './App.css';
 import Game from './Game/Game';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <div>
         <Game />
       </div>
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,50 +1,119 @@
 import React, { Component } from 'react';
-import './Game.css'
+import './Game.css';
+
 class Game extends Component {
     constructor(){
         super()
         this.state = {
             number: "",
             message: "",
-            random: generateRandomNumber(100)
+            random: "0",
+            player: "0",
+            cpu: "0",
         }
     }
 
-    handleOnChange = e => {
+    handleOnClick = e => {
         const {target: {value}} = e;
-        console.log(value);
-        if(value.trim() > 0){
-            this.setState({
-                number: value,
-            });
-        }
+        const newnumber = generateRandomNumber(3);
         this.setState({
-            message: "",
+            number: value,
+            random: newnumber,
         })
-    }
-
-    handleOnClick = () => {
+        console.log(this.state.number);
+        console.log(this.state.random);
         const number = parseInt(this.state.number);
         const random = parseInt(this.state.random);
-        const text = calculateText(number, random);
+        let text = calculateText(number, random);
         this.setState({
             message: text,
         })
+        //------------------
+        if(number == 1){
+            if(random == 2){
+                this.setState({
+                    cpu: parseInt(this.state.cpu) + 1,
+                })
+            }
+            else if(random == 3){
+                this.setState({
+                    player: parseInt(this.state.player) + 1,
+                })
+            }
+        }
+        else if(number == 2){
+            if(random == 1){
+                this.setState({
+                    player: parseInt(this.state.player) + 1,
+                })
+            }
+            else if(random == 3){
+                this.setState({
+                    cpu: parseInt(this.state.cpu) + 1,
+                })
+            }
+        }
+        else if(number == 3){
+            if(random == 2){
+                this.setState({
+                    player: parseInt(this.state.player) + 1,
+                })
+            }
+            else if(random == 1){
+                this.setState({
+                    cpu: parseInt(this.state.cpu) + 1,
+                })
+            }
+        }
+        if(parseInt(this.state.cpu) == 2 || parseInt(this.state.player) == 2){
+            text = winner(parseInt(this.state.player), parseInt(this.state.cpu));
+            this.setState({
+                cpu: "0",
+                player: "0",
+                message: text,
+            })
+        }
     }
     render() {
-        console.log(this.state.random);
         return (
             <div className="Game">
-                <p className="titulo">Adivina el numero que estoy pensando entre el 1 - 100</p>
+                <p className="titulo">Piedra, papel o tijeras</p>
                 <input 
-                    type="number"
-                    value = {this.state.number}
-                    onChange = {this.handleOnChange}
-                    className="textoo"
+                    type="image"
+                    name="piedra"
+                    src="https://cdn77.pressenza.com/wp-content/uploads/2017/11/2417C4AC-F2BE-4A94-817A-CFF04360DEE9-720x540.png"
+                    value = "1"
+                    width = "200px"
+                    //onChange = {this.handleOnChange}
+                    onClick = {this.handleOnClick}
+                    className="piedraa"
+                    display="inline-block"
                 />
-                <br/>
-                <button onClick={this.handleOnClick} className="botoon">Probar</button>
+                <input 
+                    type="image"
+                    name="papel"
+                    src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Paper_sheet.jpg"
+                    value = "2"
+                    width = "150px"
+                    //onChange = {this.handleOnChange}
+                    onClick = {this.handleOnClick}
+                    className="papell"
+                    display="inline-block"
+                />
+                <input 
+                    type="image"
+                    name="tijera"
+                    src="https://www.truper.com/media/product/3a9/tijera-para-oficina-6-1-2-de-acero-inoxidable-d05.jpg"
+                    value = "3"
+                    width = "170px"
+                    //onChange = {this.handleOnChange}
+                    onClick = {this.handleOnClick}
+                    className="tijeraa"
+                />
                 <p className={(this.state.message) && 'resultado'}>{this.state.message }</p>
+                <br/>
+                <p>Jugador: {this.state.player}</p>
+                <p>Computadora: {this.state.cpu}</p>
             </div>
         );
     }
@@ -56,20 +125,41 @@ function generateRandomNumber(max, min = 1){
     return Math.floor(Math.random() * (max-min) + min);
 }
 
+function winner(jugador, maquina){
+    if(jugador == 2){
+        return "Has ganado :D"
+    }
+    else if(maquina == 2){
+        return "Has perdido UnU"
+    }
+}
+
 function calculateText(number, random){
-    if(number === random){
-        return "Felicidades, has acertado"
+    if(number == random){
+        return "Empate hermano"
     }
-    else if((number - random) <= 3 && (number - random) > 0){
-        return "Un poco mas hacia abajo"
+    else if(number == 1){
+        if(random == 2){
+            return "Punto para la computadora"
+        }
+        else{
+            return "Punto para el jugador"
+        }
     }
-    else if((random - number) <= 3 && (random - number) > 0){
-        return "Un poco mas hacia arriba"
+    else if(number == 2){
+        if(random == 1){
+            return "Punto para el jugador"
+        }
+        else{
+            return "Punto para la computadora"
+        }
     }
-    else if(number < random){
-        return "Mas hacia arriba"
-    }
-    else if(number > random){
-        return "Mas hacia abajo"
+    else if(number == 3){
+        if(random == 2){
+            return "Punto para el jugador";
+        }
+        else{
+            return "Punto para la computadora";
+        }
     }
 }
